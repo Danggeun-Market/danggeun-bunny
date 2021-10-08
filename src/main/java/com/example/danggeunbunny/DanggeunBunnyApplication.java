cdb51c3f70ff09bc65ac1aad024185329aa1cc4d
세현 <lyutvs@gmail.com>
2021-10-08 15:04:53 +0900

    ⚙ :: Init Project

---
+++ src/main/java/com/example/danggeunbunny/DanggeunBunnyApplication.java
@@ -0,0 +1,13 @@
+package com.example.danggeunbunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanggeunBunnyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanggeunBunnyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/danggeunbunny/DanggeunBunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danggeunbunny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanggeunBunnyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
