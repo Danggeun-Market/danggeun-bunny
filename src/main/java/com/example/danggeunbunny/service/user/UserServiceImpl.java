2f7f5c9f9f974b3a7ab8dae6246ef5e0018e63f0
세현 <lyutvs@gmail.com>
2021-10-10 01:14:32 +0900

    ⚡ :: 회원가입 기능 구현

--- src/main/java/com/example/danggeunbunny/service/user/UserServiceImpl.java
+++ src/main/java/com/example/danggeunbunny/service/user/UserServiceImpl.java
@@ -1,2 +1,19 @@
-package com.example.danggeunbunny.service.user;public class UserServiceImpl {
+package com.example.danggeunbunny.service.user;
+
+import com.example.danggeunbunny.model.user.UserRequest;
+import com.example.danggeunbunny.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void registrationUser(UserRequest userRequest) {
+        userRepository.save(UserRequest);
+
+    }
 }
