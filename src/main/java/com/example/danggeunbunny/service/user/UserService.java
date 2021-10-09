19bbafa6c37d9c2fcc0d8122442344bbd2b22370
세현 <lyutvs@gmail.com>
2021-10-10 01:15:30 +0900

    ⚡ :: 회원가입 구현

--- src/main/java/com/example/danggeunbunny/service/user/UserService.java
+++ src/main/java/com/example/danggeunbunny/service/user/UserService.java
@@ -1,2 +1,12 @@
-package com.example.danggeunbunny.service.user;public interface UserService {
+package com.example.danggeunbunny.service.user;
+
+import com.example.danggeunbunny.model.user.UserRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+    public void registrationUser(UserRequest userRequest);
+
+
 }
