4777c0615639e45d82e7aa8deb030340f955319b
세현 <lyutvs@gmail.com>
2021-10-10 20:11:41 +0900

    ⚡ :: 로그인 된 사용자 정보 가져오기 위한 어노테이션

--- src/main/java/com/example/danggeunbunny/annotation/login/LoginUser.java
+++ src/main/java/com/example/danggeunbunny/annotation/login/LoginUser.java
@@ -1,2 +1,9 @@
-package com.example.danggeunbunny.annotation.login;public @interface LoginUser {
+package com.example.danggeunbunny.annotation.login;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface LoginUser {
 }
