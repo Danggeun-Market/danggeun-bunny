d53ad9f312077d89c8a6110d8ceafa9fba85822e
세현 <lyutvs@gmail.com>
2021-10-10 14:55:50 +0900

    ♻ :: 이상한 패턴이 붙는 형식 이름 수정

--- src/main/java/com/example/danggeunbunny/service/user/UserService.java
+++ src/main/java/com/example/danggeunbunny/service/user/UserService.java
@@ -1,12 +1,12 @@
 package com.example.danggeunbunny.service.user;
 
-import com.example.danggeunbunny.model.user.UserRequest;
+import com.example.danggeunbunny.model.user.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
 
-    public void registrationUser(UserRequest userRequest);
+    public void registrationUser(User userRequest);
 
     public boolean isDuplicatedEMail(String email);
 
