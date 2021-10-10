8bb9ad36d61f268d76e78b1a2f5a18b95b15f961
세현 <lyutvs@gmail.com>
2021-10-10 15:49:41 +0900

    ⚡ :: 이메일을 사용한 사용자 검색기능 추가

--- src/main/java/com/example/danggeunbunny/service/user/GeneralUserServiceImpl.java
+++ src/main/java/com/example/danggeunbunny/service/user/GeneralUserServiceImpl.java
@@ -1,24 +1,33 @@
 package com.example.danggeunbunny.service.user;
 
+import com.example.danggeunbunny.exception.user.MemberNotFoundException;
 import com.example.danggeunbunny.model.user.User;
 import com.example.danggeunbunny.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class GeneralUserServiceImpl implements UserService{
 
-    private final UserRepository userRepository;
+
+    private final UserRepository memberRepository;
 
     @Override
+    @Transactional
     public void registrationUser(User user) {
-        userRepository.save(user);
-
+        memberRepository.save(user);
     }
 
     @Override
-    public boolean isDuplicatedEMail(String email) {
-        return userRepository.existsByEmail(email);
+    public boolean isDuplicatedEmail(String email) {
+        return memberRepository.existsByEmail(email);
     }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return memberRepository.findUserByEmail(email).orElseThrow(MemberNotFoundException::new);
+    }
+
 }
