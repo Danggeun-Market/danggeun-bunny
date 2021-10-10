72c23466e554a76886013c33013fa3279cda001d
세현 <lyutvs@gmail.com>
2021-10-10 19:12:29 +0900

    ⚡ :: 사용자 프로필 정보 변경요청을 위한 객체 생성

--- src/main/java/com/example/danggeunbunny/dto/profile/ProfileRequestDto.java
+++ src/main/java/com/example/danggeunbunny/dto/profile/ProfileRequestDto.java
@@ -1,2 +1,16 @@
-package com.example.danggeunbunny.dto.profile;public class ProfileRequestDto {
+package com.example.danggeunbunny.dto.profile;
+
+import jdk.jfr.BooleanFlag;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@BooleanFlag
+@Getter
+@RequiredArgsConstructor
+public class ProfileRequestDto {
+
+    private final String email;
+
+    private final String nickname;
+    
 }
