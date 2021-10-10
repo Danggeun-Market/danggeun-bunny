ea1e3d9676f9e24e8d571dd205744e7f5a703f49
세현 <lyutvs@gmail.com>
2021-10-10 14:39:42 +0900

    ♻ :: 대괄호 실수 넣음

--- src/main/java/com/example/danggeunbunny/util/HttpStatusResponseEntity.java
+++ src/main/java/com/example/danggeunbunny/util/HttpStatusResponseEntity.java
@@ -14,5 +14,3 @@ public class HttpStatusResponseEntity {
     public static final ResponseEntity<HttpStatus> RESPONSE_PAYLOAD_TOO_LARGE = ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).build();
 }
 
-
-}
