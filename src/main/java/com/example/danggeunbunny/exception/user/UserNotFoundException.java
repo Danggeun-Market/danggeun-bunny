a8c2825145a18855199aaaf53b0a25bf250eb1e3
세현 <lyutvs@gmail.com>
2021-10-10 15:48:57 +0900

    📑 :: 사용자 찾지 못했을때 발생시킬 런타임 에러

---
+++ src/main/java/com/example/danggeunbunny/exception/user/UserNotFoundException.java
@@ -0,0 +1,16 @@
+package com.example.danggeunbunny.exception.user;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException() {
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
