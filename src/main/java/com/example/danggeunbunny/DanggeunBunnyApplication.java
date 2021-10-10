5c6ffb060b17d7481d3fbb8466820fe38c36365b
세현 <lyutvs@gmail.com>
2021-10-10 15:54:48 +0900

    ⚡ :: Spring Session을 Redis 에서 지원

--- src/main/java/com/example/danggeunbunny/DanggeunBunnyApplication.java
+++ src/main/java/com/example/danggeunbunny/DanggeunBunnyApplication.java
@@ -2,8 +2,10 @@ package com.example.danggeunbunny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class DanggeunBunnyApplication {
 
     public static void main(String[] args) {
