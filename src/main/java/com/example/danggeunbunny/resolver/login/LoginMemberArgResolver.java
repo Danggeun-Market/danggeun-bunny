2d0e2c5e0af27989ce4a203fc44fe14e0676e6af
세현 <lyutvs@gmail.com>
2021-10-10 20:15:48 +0900

    📑 :: LoginMemberArgResolver

---
+++ src/main/java/com/example/danggeunbunny/resolver/login/LoginMemberArgResolver.java
@@ -0,0 +1,28 @@
+package com.example.danggeunbunny.resolver.login;
+
+import com.example.danggeunbunny.annotation.login.LoginUser;
+import com.example.danggeunbunny.service.login.LoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final LoginService loginService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return methodParameter.hasParameterAnnotation(LoginUser.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return loginService.getLoginUser();
+    }
+}
