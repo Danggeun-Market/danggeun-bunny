eb0ed8c84a2c7351d95cb6c1c42e0fad24cfb82b
세현 <lyutvs@gmail.com>
2021-10-11 20:40:29 +0900

    ⚡ :: 주소 확인을 위한 임베디드 구현

--- src/main/java/com/example/danggeunbunny/model/address/Address.java
+++ src/main/java/com/example/danggeunbunny/model/address/Address.java
@@ -1,2 +1,23 @@
-package com.example.danggeunbunny.model.address;public class Address {
+package com.example.danggeunbunny.model.address;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Address {
+
+    private String depth1;
+
+    private String depth2;
+
+    private String depth3;
+    
 }
