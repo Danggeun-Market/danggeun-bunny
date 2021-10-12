a6e131a9569edbfc52e0e467787df1cc2d615cb0
세현 <lyutvs@gmail.com>
2021-10-12 15:40:16 +0900

    ⚡ :: 사용자 위치 정보를 주고받을 클래스 선언

--- src/main/java/com/example/danggeunbunny/dto/location/LocationRequestDto.java
+++ src/main/java/com/example/danggeunbunny/dto/location/LocationRequestDto.java
@@ -1,2 +1,33 @@
-package com.example.danggeunbunny.dto.location;public class locationRequestDto {
+package com.example.danggeunbunny.dto.location;
+
+import com.example.danggeunbunny.model.address.Address;
+import com.example.danggeunbunny.model.address.Location;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class LocationRequestDto {
+
+    private final String state;
+    private final String city;
+    private final String town;
+    private final Double longitude;
+    private final Double latitude;
+
+    public Address toAddress() {
+        return Address.builder()
+                .state(state)
+                .city(city)
+                .town(town)
+                .build();
+    }
+
+    public Location toLocation() {
+        return Location.builder()
+                .longitude(longitude)
+                .latitude(latitude)
+                .build();
+    }
+
 }
