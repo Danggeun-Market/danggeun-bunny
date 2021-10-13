6e1b49a3989cf587865605551767e271fcaf47ed
세현 <lyutvs@gmail.com>
2021-10-13 16:16:27 +0900

    🧪 :: 사용자 프로필 및 패스워드 변경, 지역 정보 추가

--- src/test/java/com/example/danggeunbunny/service/user/UserServiceTest.java
+++ src/test/java/com/example/danggeunbunny/service/user/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.danggeunbunny.service.user;
 
+import com.example.danggeunbunny.dto.location.LocationRequestDto;
 import com.example.danggeunbunny.dto.profile.PasswordRequestDto;
+import com.example.danggeunbunny.dto.profile.ProfileRequestDto;
 import com.example.danggeunbunny.dto.user.UserDto;
 import com.example.danggeunbunny.exception.user.UserNotFoundException;
 import com.example.danggeunbunny.model.user.User;
@@ -39,6 +41,10 @@ class UserServiceTest {
 
     private PasswordRequestDto passwordRequestDto;
 
+    private ProfileRequestDto profileRequestDto;
+
+    private LocationRequestDto locationRequestDto;
+
     @BeforeEach
     void setUp() {
         when(passwordEncoder.encode(any())).thenReturn("pillar0030!");
@@ -49,6 +55,17 @@ class UserServiceTest {
                 .build();
 
         passwordRequestDto = new PasswordRequestDto("pillar0030!", "PILLAR))#)!");
+
+        profileRequestDto = new ProfileRequestDto("lyutvs@gmail.com", "엄청난노옴");
+
+        locationRequestDto = LocationRequestDto.builder()
+                .state("대전광역시")
+                .city("여성구")
+                .town("중구")
+                .longitude(126.94250287828)
+                .latitude(37.4853777674734)
+                .build();
+
 
         user = UserDto.toEntity(userDto, passwordEncoder);
     }
@@ -143,5 +160,49 @@ class UserServiceTest {
 
         // then
         assertFalse(generalUserService.isValidPassword(user, passwordRequestDto, passwordEncoder));
+    }
+
+    @Test
+    @DisplayName("사용자 프로필 변경 성공한 경우")
+    void successToUpdateUserProfile() {
+
+        // when
+        generalUserService.updateUserProfile(user, profileRequestDto);
+
+        // then
+        assertEquals(user.getNickname(), profileRequestDto.getNickname());
+
+    }
+
+    @Test
+    @DisplayName("사용자 패스워드 변경에 성공한 경우")
+    void successToUpdatePassword() {
+
+        // given
+        when(passwordEncoder.encode(any())).thenReturn(passwordRequestDto.getNewPassword());
+
+        // when
+        generalUserService.updateUserPassword(user, passwordRequestDto, passwordEncoder);
+
+        // then
+        assertEquals(user.getPassword(), passwordRequestDto.getOldPassword());
+
+    }
+
+    @Test
+    @DisplayName("사용자 위치 정보 등록에 성공한 경우")
+    void successToUpdateUserLocationAndAddress() {
+
+        // whenn
+        generalUserService.setUserLocationAddress(user, locationRequestDto);
+
+        // then
+        assertThat(user.getAddress()).isNotNull();
+        assertThat(user.getLocation()).isNotNull();
+        assertEquals(user.getAddress().getState(), locationRequestDto.getState());
+        assertEquals(user.getAddress().getCity(), locationRequestDto.getCity());
+        assertEquals(user.getAddress().getTown(), locationRequestDto.getTown());
+        assertEquals(user.getLocation().getLongitude(), locationRequestDto.getLongitude());
+        assertEquals(user.getLocation().getLatitude(), locationRequestDto.getLatitude());
     }
 }
