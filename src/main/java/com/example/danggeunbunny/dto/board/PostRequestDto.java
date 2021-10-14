553f06ee356b70f0f892977492190a0709118bb5
세현 <lyutvs@gmail.com>
2021-10-14 15:04:28 +0900

    ⚡ :: POST로 등록 받을 객체 생성

--- src/main/java/com/example/danggeunbunny/dto/board/PostRequestDto.java
+++ src/main/java/com/example/danggeunbunny/dto/board/PostRequestDto.java
@@ -1,2 +1,23 @@
-package com.example.danggeunbunny.dto.board;public class PostRequestDto {
+package com.example.danggeunbunny.dto.board;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@RequiredArgsConstructor
+public class PostRequestDto {
+
+    @NotEmpty
+    @Length(max = 100, message = "제목은 최대 100글자를 넘을 수 없습니다.")
+    private final String title;
+
+    @NotEmpty
+    private final String content;
+
+    @NotEmpty
+    private final String category;
+
 }
