418e7a4771bea5ec509bd52dc1f18ecd303e6273
세현 <lyutvs@gmail.com>
2021-10-15 11:29:33 +0900

    ⚡ :: 파일 형식 interface로 안 바꿈...

--- src/main/java/com/example/danggeunbunny/repository/Post/PostRepository.java
+++ src/main/java/com/example/danggeunbunny/repository/Post/PostRepository.java
@@ -3,5 +3,5 @@ package com.example.danggeunbunny.repository.Post;
 import com.example.danggeunbunny.model.board.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
