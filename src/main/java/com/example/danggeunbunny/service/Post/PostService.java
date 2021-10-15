cc9450a23985aa83b1613274b86148f6065be6ae
세현 <lyutvs@gmail.com>
2021-10-15 11:11:19 +0900

    📑 :: PostService 파일 생성

---
+++ src/main/java/com/example/danggeunbunny/service/Post/PostService.java
@@ -0,0 +1,10 @@
+package com.example.danggeunbunny.service.Post;
+
+import com.example.danggeunbunny.model.user.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface PostService {
+
+
+}
