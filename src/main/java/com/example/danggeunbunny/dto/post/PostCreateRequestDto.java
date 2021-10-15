5dbed488421f51a8194873a32a7c959f26c21c72
세현 <lyutvs@gmail.com>
2021-10-15 11:13:58 +0900

    📑 ::  좀더 가독성 있는 이름

--- src/main/java/com/example/danggeunbunny/dto/post/PostCreateRequestDto.java
+++ src/main/java/com/example/danggeunbunny/dto/post/PostCreateRequestDto.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotEmpty;
 
 @Getter
 @RequiredArgsConstructor
-public class PostRequestDto {
+public class PostCreateRequestDto {
 
     @NotEmpty
     @Length(max = 100, message = "제목은 최대 100글자를 넘을 수 없습니다.")
