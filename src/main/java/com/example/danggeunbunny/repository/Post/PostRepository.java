bd9eb5f232d5d05b0622b5615e0927d4c8cbe494
세현 <lyutvs@gmail.com>
2021-10-15 11:11:00 +0900

    📑 :: 개시판 레포지토리

---
+++ src/main/java/com/example/danggeunbunny/repository/Post/PostRepository.java
@@ -0,0 +1,7 @@
+package com.example.danggeunbunny.repository.board;
+
+import com.example.danggeunbunny.model.board.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class PostRepository extends JpaRepository<Post, Long> {
+}
