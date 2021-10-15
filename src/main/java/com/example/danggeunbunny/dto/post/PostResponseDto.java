bc5b82d082d169062ccc6c72e0e64cf8ef4e6471
세현 <lyutvs@gmail.com>
2021-10-15 14:01:12 +0900

    ⚡ :: 사용자가 특정 게시물 조회시 조회 결과 반환

--- src/main/java/com/example/danggeunbunny/dto/post/PostResponseDto.java
+++ src/main/java/com/example/danggeunbunny/dto/post/PostResponseDto.java
@@ -1,2 +1,47 @@
-package com.example.danggeunbunny.dto.post;public class PostResponseDto {
+package com.example.danggeunbunny.dto.post;
+
+import com.example.danggeunbunny.model.address.Address;
+import com.example.danggeunbunny.model.address.Location;
+import com.example.danggeunbunny.model.board.entity.Post;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@RequiredArgsConstructor
+public class PostResponseDto {
+
+    private final Long id;
+    private final String title;
+    private final String author;
+    private final String email;
+    private final String content;
+
+    private final String status;
+    private final String category;
+
+    private final Address address;
+    private final Location location;
+
+    private final LocalDateTime createdTime;
+    private final LocalDateTime modifiedTime;
+
+    public static PostResponseDto of(Post post) {
+        return PostResponseDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .author(post.getAuthor().getNickname())
+                .email(post.getAuthor().getEmail())
+                .content(post.getContent())
+                .createdTime(post.getCreatedTime())
+                .modifiedTime(post.getModifiedTime())
+                .status(post.getStatus().getTradeStatus())
+                .category(post.getCategory().getCategoryName())
+                .address(post.getAddress())
+                .location(post.getLocation())
+                .build();
+    }
 }
