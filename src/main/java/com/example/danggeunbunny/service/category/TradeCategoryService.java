915f3292c5990018100ee3afe7ce529f38abc1b0
세현 <lyutvs@gmail.com>
2021-10-15 21:57:47 +0900

    ⚡ :: 카테고리 조회 기능 및 캐싱

--- src/main/java/com/example/danggeunbunny/service/category/TradeCategoryService.java
+++ src/main/java/com/example/danggeunbunny/service/category/TradeCategoryService.java
@@ -1,4 +1,23 @@
 package com.example.danggeunbunny.service.category;
 
-public class TradeCatergoryService {
+import com.example.danggeunbunny.exception.board.CategoryNotFoundException;
+import com.example.danggeunbunny.model.board.post.Category;
+import com.example.danggeunbunny.repository.category.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TradeCategoryService implements CategoryService{
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    @Cacheable(key = "#categoryName", value = "category")
+    public Category findCategoryByName(String categoryName) {
+        return categoryRepository.findCategoryByCategoryName(categoryName)
+                .orElseThrow(() -> new CategoryNotFoundException(categoryName));
+
+    }
 }
