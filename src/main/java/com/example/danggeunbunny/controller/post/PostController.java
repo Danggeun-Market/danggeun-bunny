d68ebef6df282973174c8261a45425301eae2144
세현 <lyutvs@gmail.com>
2021-10-15 11:23:10 +0900

    ⚡ :: r거래 게시물 등록 기능

--- src/main/java/com/example/danggeunbunny/controller/post/PostController.java
+++ src/main/java/com/example/danggeunbunny/controller/post/PostController.java
@@ -1,2 +1,35 @@
-package com.example.danggeunbunny.controller.post;public class PostController {
+package com.example.danggeunbunny.controller.post;
+
+import com.example.danggeunbunny.annotation.login.LoginRequired;
+import com.example.danggeunbunny.annotation.login.LoginUser;
+import com.example.danggeunbunny.dto.post.PostCreateRequestDto;
+import com.example.danggeunbunny.model.user.User;
+import com.example.danggeunbunny.service.Post.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import static com.example.danggeunbunny.util.HttpStatusResponseEntity.RESPONSE_OK;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    @LoginRequired
+    @PostMapping
+    public ResponseEntity<HttpStatus> creatNewPost(@RequestBody @Valid PostCreateRequestDto postCreateRequestDto, @LoginUser User user) {
+
+        postService.createNewPost(postCreateRequestDto, user);
+
+        return RESPONSE_OK;
+    }
 }
