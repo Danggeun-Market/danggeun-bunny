98d0ad6956df16278740b2e0f3cfeec1a99b3be0
세현 <lyutvs@gmail.com>
2021-10-18 11:26:49 +0900

    ⚡ :: 각각의 캐시 이름과 만료 시간

--- src/main/java/com/example/danggeunbunny/enumeration/CacheKey.java
+++ src/main/java/com/example/danggeunbunny/enumeration/CacheKey.java
@@ -1,2 +1,14 @@
-package com.example.danggeunbunny.enumeration;public class CacheKey {
+package com.example.danggeunbunny.enumeration;
+
+import java.time.Duration;
+
+public class CacheKey {
+
+    public static final String POST = "POST";
+
+    public static final String CATEGORY = "CATEGORY";
+
+    public static final Duration POST_CACHE_EXPIRE_TIME = Duration.ofMinutes(5L);
+    
+
 }
