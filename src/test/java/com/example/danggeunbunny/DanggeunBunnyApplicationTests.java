c98ce1f193bd5f983be211c3d41537def352e2aa
세현 <lyutvs@gmail.com>
2021-10-19 17:08:49 +0900

    ⚡ :: 통합 테스트

--- src/test/java/com/example/danggeunbunny/DanggeunBunnyApplicationTests.java
+++ src/test/java/com/example/danggeunbunny/DanggeunBunnyApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.danggeunbunny;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DanggeunBunnyApplicationTests {
 
     @Test
