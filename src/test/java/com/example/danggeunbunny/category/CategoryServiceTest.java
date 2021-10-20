bc9944c70b1841ee28df06bbd0cbe87c2e1cd292
세현 <lyutvs@gmail.com>
2021-10-20 23:01:44 +0900

    🧪 :: 임포트 삭제

--- src/test/java/com/example/danggeunbunny/category/CategoryServiceTest.java
+++ src/test/java/com/example/danggeunbunny/category/CategoryServiceTest.java
@@ -13,7 +13,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static javax.print.attribute.TextSyntax.verify;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
