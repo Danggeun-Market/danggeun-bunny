54cc8a008435090c1d4ce0174fd5b784046785ab
세현 <lyutvs@gmail.com>
2021-10-24 23:01:10 +0900

    ⚡ :: 어노테이션 충돌 방지

--- src/main/java/com/example/danggeunbunny/DanggeunBunnyApplication.java
+++ src/main/java/com/example/danggeunbunny/DanggeunBunnyApplication.java
@@ -2,13 +2,10 @@ package com.example.danggeunbunny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableRedisHttpSession
 public class DanggeunBunnyApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/danggeunbunny/config/jpa/JpaAuditingConfig.java
+++ src/main/java/com/example/danggeunbunny/config/jpa/JpaAuditingConfig.java
@@ -1,2 +1,9 @@
-package com.example.danggeunbunny.config.jpa;public class JpaAuditingConfig {
+package com.example.danggeunbunny.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
 }
