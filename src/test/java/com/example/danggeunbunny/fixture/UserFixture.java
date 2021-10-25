eab1e1840662a544e6a0df3053017b710bb85a20
세현 <lyutvs@gmail.com>
2021-10-25 22:28:56 +0900

    ⚡ :: 테스트때 필요한 것 상수로 저장

--- src/test/java/com/example/danggeunbunny/fixture/UserFixture.java
+++ src/test/java/com/example/danggeunbunny/fixture/UserFixture.java
@@ -1,4 +1,39 @@
 package com.example.danggeunbunny.fixture;
 
+import com.example.danggeunbunny.dto.user.UserDto;
+import com.example.danggeunbunny.model.user.User;
+
 public class UserFixture {
+
+
+    public static final User ADMIN_User = new User(
+            "admin@daangnmarket.com",
+            "!Admin123",
+            "admin"
+    );
+
+    public static final User User1 = new User(
+            "user1@daangnmarket.com",
+            "!User123",
+            "user1"
+    );
+
+    public static final User User2 = new User(
+            "user2@daangnmarket.com",
+            "!User123",
+            "user2"
+    );
+
+    public static final UserDto User_REGISTRATION_REQUEST = new UserDto(
+            "newuser@daangnmarket.com",
+            "!Newuser123",
+            "newUser"
+    );
+
+    public static final User NEW_User = new User(
+            "newuser@daangnmarket.com",
+            "!Newuser123",
+            "newUser"
+    );
+
 }
