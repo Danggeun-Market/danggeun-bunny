307baccd73483d9af36e30e3f9d805d05c841ed6
세현 <lyutvs@gmail.com>
2021-10-26 12:31:28 +0900

    ⚡ :: 좀더 가독성 있는 글자로 바꿈

--- src/test/java/com/example/danggeunbunny/fixture/UserFixture.java
+++ src/test/java/com/example/danggeunbunny/fixture/UserFixture.java
@@ -6,31 +6,31 @@ import com.example.danggeunbunny.model.user.User;
 public class UserFixture {
 
 
-    public static final User ADMIN_User = new User(
+    public static final User ADMIN_USER = new User(
             "admin@daangnmarket.com",
             "!Admin123",
             "admin"
     );
 
-    public static final User User1 = new User(
+    public static final User USER1 = new User(
             "user1@daangnmarket.com",
             "!User123",
             "user1"
     );
 
-    public static final User User2 = new User(
+    public static final User USER2 = new User(
             "user2@daangnmarket.com",
             "!User123",
             "user2"
     );
 
-    public static final UserDto User_REGISTRATION_REQUEST = new UserDto(
+    public static final UserDto USER_REGISTRATION_REQUEST = new UserDto(
             "newuser@daangnmarket.com",
             "!Newuser123",
             "newUser"
     );
 
-    public static final User NEW_User = new User(
+    public static final User NEW_USER = new User(
             "newuser@daangnmarket.com",
             "!Newuser123",
             "newUser"
