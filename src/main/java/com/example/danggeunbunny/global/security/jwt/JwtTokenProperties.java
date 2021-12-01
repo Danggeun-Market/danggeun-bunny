c80886bfe6ac81d919ad7855129d739e47260a52
세현 <lyutvs@gmail.com>
2021-12-01 22:11:33 +0900

    ⚡ :: JwtTokenProperties

--- src/main/java/com/example/danggeunbunny/global/security/jwt/JwtTokenProperties.java
+++ src/main/java/com/example/danggeunbunny/global/security/jwt/JwtTokenProperties.java
@@ -1,2 +1,28 @@
-package com.example.danggeunbunny.global.security.jwt;public class JwtTokenProperties {
+package com.example.danggeunbunny.global.security.jwt;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import java.util.Base64;
+
+@Getter
+@ConstructorBinding
+@ConfigurationProperties(prefix = "auth.jwt")
+public class JwtTokenProperties {
+
+    private final String secret;
+    private final String header;
+    private final String prefix;
+    private final Long access;
+    private final Long refresh;
+
+    public JwtTokenProperties(String secret, Long access, Long refresh,
+                         String header, String prefix) {
+        this.secret = Base64.getEncoder().encodeToString(secret.getBytes());
+        this.access = access;
+        this.refresh = refresh;
+        this.header = header;
+        this.prefix = prefix;
+    }
 }
