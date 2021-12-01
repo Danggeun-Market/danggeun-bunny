6e0abd5b13cc1ad7f37436c4f3c2e834f42c7ed2
세현 <lyutvs@gmail.com>
2021-12-01 22:23:26 +0900

    📑 :: AuthUserDetailsService

---
+++ src/main/java/com/example/danggeunbunny/global/security/auth/AuthUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.danggeunbunny.global.security.auth;
+
+import com.example.danggeunbunny.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)  throws UsernameNotFoundException {
+        return userRepository.findById(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+    }
+}
